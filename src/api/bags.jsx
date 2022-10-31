0e4c1cd8042d8a302de696c523d7e113f78a7202
seu-usersaneme <seu-email>
2022-10-31 17:09:46 -0300

    localhost

--- src/api/bags.jsx
+++ src/api/bags.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getBagsRequests = async () =>
-  await axios.get("https://bag-server.onrender.com/bag", { headers: {'Content-Type': 'application/octet-stream'}});
+  await axios.get("http://localhost:4000/bag");
 
 
 export const createBagRequest = async (bag) => {
